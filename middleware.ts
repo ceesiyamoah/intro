868bef2348e170c9b06ffe77223e36499b2e6882
Cyril <ccyamoah@gmail.com>
2024-04-09 17:35:40 +0000

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-// import { NextResponse } from "next/server"
-// import type { NextRequest } from "next/server"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
-// const middleware = (request: NextRequest) => {
-//   return NextResponse.redirect(new URL("/", request.url))
-// }
+const middleware = (request: NextRequest) => {
+  return NextResponse.redirect(new URL("/", request.url))
+}
 
-// export const config = {
-//   matcher: "/docs",
-// }
+export const config = {
+  matcher: "/docs1",
+}
 
-// export default middleware
+export default middleware
